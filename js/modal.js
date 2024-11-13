04f26f33b0d87442bfc0ca872ef9d569e3a40d32
OlegShinka <volkonxxl@gmail.com>
2024-11-13 22:50:52 +0200

    subscribe/review

--- js/modal.js
+++ js/modal.js
@@ -1,32 +1,49 @@
+(() => {
+  const modals = {
+    modal1: {
+      openBtns: document.querySelectorAll("[data-modal-open='modal1']"),
+      closeBtn: document.querySelector("[data-modal-close='modal1']"),
+      modal: document.querySelector("[data-modal='modal1']"),
+    },
+    modal2: {
+      openBtns: document.querySelectorAll("[data-modal-open='modal2']"),
+      closeBtn: document.querySelector("[data-modal-close='modal2']"),
+      modal: document.querySelector("[data-modal='modal2']"),
+    },
+    modal3: {
+      openBtns: document.querySelectorAll("[data-modal-open='modal3']"),
+      closeBtn: document.querySelector("[data-modal-close='modal3']"),
+      modal: document.querySelector("[data-modal='modal3']"),
+    },
+  };
+
+  Object.values(modals).forEach(({ openBtns, closeBtn, modal }) => {
+    openBtns.forEach((btn) => {
+      btn.addEventListener("click", () => toggleModal(modal));
+    });
+
+    closeBtn.addEventListener("click", () => toggleModal(modal));
+  });
+
+  function toggleModal(modal) {
+    modal.classList.toggle("is-hidden");
+  }
+})();
+
 // (() => {
 //   const refs = {
-//     openModalBtn: document.querySelector("[data-modal-open]"),
+//     openModalBtns: document.querySelectorAll("[data-modal-open]"),
 //     closeModalBtn: document.querySelector("[data-modal-close]"),
 //     modal: document.querySelector("[data-modal]"),
 //   };
 
-//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.openModalBtns.forEach((btn) => {
+//     btn.addEventListener("click", toggleModal);
+//   });
+
 //   refs.closeModalBtn.addEventListener("click", toggleModal);
 
 //   function toggleModal() {
 //     refs.modal.classList.toggle("is-hidden");
 //   }
 // })();
-
-(() => {
-  const refs = {
-    openModalBtns: document.querySelectorAll("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
-
-  refs.openModalBtns.forEach((btn) => {
-    btn.addEventListener("click", toggleModal);
-  });
-
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
--- js/script.js
+++ js/script.js
@@ -1,23 +1,18 @@
-const swiper = new Swiper(".swiper", {
+const swiper1 = new Swiper("#products-swiper", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
-
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-
   // slideToClickedSlide: true,
-
   grabCursor: true,
-
-  mousewheel: {
-    sensitivity: 1,
-    eventsTarget: ".pic-slide",
-  },
-
+  // mousewheel: {
+  //   sensitivity: 1,
+  //   eventsTarget: ".pic-slide",
+  // },
   breakpoints: {
     320: {
       slidesPerView: 1,
@@ -29,27 +24,50 @@ const swiper = new Swiper(".swiper", {
       slidesPerView: 4,
     },
   },
-
   //   кількість відображених слайдів  можливо показувати не цілу кільк. слайдів
   //   slidesPerView: 4,
-
   // відстань між слайдами
   spaceBetween: 18,
-
   // нескінченна прокрутка слайдера
   loop: true,
-
   //   автовисота слайду
   // autoHeight: true,
-
   // Navigation arrows
   //   navigation: {
   //     nextEl: ".swiper-button-next",
   //     prevEl: ".swiper-button-prev",
   //   },
-
   // And if we need scrollbar
   //   scrollbar: {
   //     el: ".swiper-scrollbar",
   //   },
+});
+
+const swiper2 = new Swiper("#reviews-swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  grabCursor: true,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    720: {
+      slidesPerView: 2.5,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+  // відстань між слайдами
+  spaceBetween: 28,
+  // нескінченна прокрутка слайдера
+  loop: true,
 });
